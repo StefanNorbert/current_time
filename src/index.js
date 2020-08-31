8bb77f0ecfba7dc5f0e7e174c9d920458c8b1766
Stefan <iuhasstefannorbert@yahoo.com>
2020-08-31 17:46:36 +0300

    install Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+// this will allow the use of bootstrap classes
+import 'bootstrap/dist/css/bootstrap.min.css';
+// if you also need to use Bootstrap’s JavaScript components in your app, you’ll need to install jquery and popper.js
+// https://blog.logrocket.com/how-to-use-bootstrap-with-react-a354715d1121/
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
